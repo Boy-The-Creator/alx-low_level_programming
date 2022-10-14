7c6164aec3805d1a10f06964b13f240f60e055d3
Boy-The-Creator <75540637+Boy-The-Creator@users.noreply.github.com>
2022-10-14 05:57:45 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  *  main - Entry point
- *   
+ *
  *  Return:1
  */
 int main(void)
@@ -10,5 +10,4 @@ int main(void)
 		write(2,
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 		return (1);
-
 }
