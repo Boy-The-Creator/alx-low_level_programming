1e56abbe1cd42ac338f80624fe4424f855d947cf
Boy-The-Creator <ojeremiodunayo@gmail.com>
2022-10-21 06:50:08 -0700

    a function that prints the numbers from 0-9 on a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints the numbers.
+ * return 0
+ */
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+_putchar('\n');
+}
